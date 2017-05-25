345fbd32268f2b600705dc24dcea62341f22c68e
neilbaksi <snb.1996@gmail.com>
2017-05-26 00:52:08 +1000

    added his direction changing

--- src/Player.java
+++ src/Player.java
@@ -4,6 +4,8 @@ import javax.swing.ImageIcon;
 public class Player extends WarehouseObject {
 	
 	private Image image;
+	private Image imageR;
+	private Image imageU;
 
 	/**
 	 * Constructor for Player object.
@@ -14,6 +16,8 @@ public class Player extends WarehouseObject {
 	public Player (int x, int y){
 		super(x, y);
 		image = new ImageIcon(this.getClass().getResource("/images/player.png")).getImage();
+		imageR = new ImageIcon(this.getClass().getResource("/images/playerR.png")).getImage();
+		imageU = new ImageIcon(this.getClass().getResource("/images/playerU.png")).getImage();
 		this.setImage(image);
 	}
 	
@@ -27,15 +31,19 @@ public class Player extends WarehouseObject {
 		
 		switch(dir){
 		case UP:
+			setImage(imageU);
 			setY(this.y()-1);
 			break;
 		case DOWN:
+			setImage(image);
 			setY(this.y()+1);
 			break;
 		case LEFT:
+			setImage(image);
 			setX(this.x()-1);
 			break;
 		case RIGHT:
+			setImage(imageR);
 			setX(this.x()+1);
 			break;
 		}
